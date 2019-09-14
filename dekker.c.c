9e59064687da4dd4f90d9522d39218d3ccf5a05a
Pâmela <viingoflaas@gmail.com>
2019-09-13 21:58:20 -0300

    Dekker implememtado nos canibais(sem no cozinheiro)

--- dekker.c.c
+++ dekker.c.c
@@ -10,54 +10,16 @@ uma unica thread acessa a regiao critica por vez.
 
 #define THREADS		5
 
+sem_t semPratos;
+sem_t mutexCozinha;
 sem_t mutex;
+
 int flag;
 int interesse[THREADS];
 int vez = 0;
-
-void *task(void *arg){
-	int item, tid, i;
-
-	tid = (int)(long int)arg;
-	while(1){
-            //printf("oi  %d\n\n", vez);
-            interesse[0] = 1;//fala q tem interesse
-            while(interesse[1]){ //enquanto tiver alguem com interesse
-               if(vez != 0){ // e nao for /minha vez
-                    interesse[0] = 0; //digo q n tenho interesse
-                    while(vez != 0);// enquanto n for minha vez
-                    interesse[0] = 1; //eu aviso q tenho interesse
-               } // é minha vez
-            }
-                printf("thread %d..\n", tid);
-                 usleep(990000);
-                vez = 1;
-                interesse[0] = 0;
-                    //escolhevez();
-
-	}
-}
-void *task2(void *arg){
-	int item, tid, i;
-
-	tid = (int)(long int)arg;
-	while(1){
-            //printf("oi  %d\n\n", vez);
-            interesse[1] = 1;//fala q tem interesse
-            while(interesse[0]){ //enquanto tiver alguem com interesse
-               if(vez != 1){ // e nao for /minha vez
-                    interesse[1] = 0; //digo q n tenho interesse
-                    while(vez != 1);// enquanto n for minha vez
-                    interesse[1] = 1; //eu aviso q tenho interesse
-               }
-            }
-                   printf("thread %d\n", tid);
-                    usleep(990000);
-                    //escolhevez();
-                    vez = 0;
-                    interesse[1] = 0;
-	}
-}
+int auxvez;
+int pratos = 2;
+int cozinha = 0;
 
 void *task3(void *arg){
 	int item, tid, i;
@@ -73,15 +35,42 @@ void *task3(void *arg){
                     interesse[tid] = 1; //eu aviso q tenho interesse
                }
             }
-                   printf("thread %d\n", tid);
-                    usleep(990000);
-                    printf("Minha vez: %d\n", vez);
-                    escolhevez();
-                    printf("Amiguinho vez: %d\n", vez);
-                    //vez = 0;
-                    usleep(990000);
-                    interesse[tid] = 0;
+            //RC begin
+            printf("Thread %d\n", tid);
+            if( pratos <= 0){
+                //auxvez = vez;
+                //vez = 99;
+                printf("Acabo comida! Chamando o Cozinheiro\n");
+                cozinha = 1;
+                sem_post(&mutexCozinha);
+                while(cozinha);
+            }
+            printf("Vou comer...");
+            sem_wait(&semPratos);
+            pratos--;
+            printf("thread Comendo!\n");
+            //usleep(550000);
+            escolhevez();
+            //RC end
+            interesse[tid] = 0;
 	}
+}
+
+void *task2(void *arg){
+	int item, tid, i;
+    int vezaux = vez;
+    //vez = 99;
+	tid = (int)(long int)arg;
+	while(1){
+        sem_wait(&mutexCozinha);
+        for(int i = 0; i < 2;i++){
+            sem_post(&semPratos);
+            }
+            pratos = 2;
+            printf("\n Fazendo comida...\n");
+            cozinha = 0;
+	    }
+	    //vez = vezaux;
 }
 
 void escolhevez(){
@@ -109,13 +98,15 @@ int main(void){
 
 	for(i = 0; i < THREADS; i++)
         interesse[i] = 0;
-
-	sem_init(&mutex,0,1);
+    sem_init(&semPratos,0,pratos);
+	sem_init(&mutexCozinha,0,0);
 
     for(i = 0; i < THREADS; i++)
 		pthread_create(&threads[i], NULL, task3, (void *)i);
 		//pthread_create(&threads[1], NULL, task2, (void *)1);
 
+    pthread_t cozinha;
+		pthread_create(&cozinha, NULL, task2, (void *)i);
 	pthread_exit(NULL);
 	return(0);
 }
